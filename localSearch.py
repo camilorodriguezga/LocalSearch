55357456b48badb40dcd6a23fd30d6ddc6800dc5
Camilo Andrés Rodríguez Garzón <cam_rod@hotmail.com>
2018-05-30 08:56:28 -0500

    refactor code swap move

--- localSearch.py
+++ localSearch.py
@@ -18,7 +18,8 @@ class TimeTabling:
 		self.slot = s
 
 	def __repr__(self):
-		return "{event : %s, area : %s, block : %s,  slot : %s-%s} \n" % (self.getEvent(), self.getArea(), self.getBlock(), self.getSlot()[0], self.getSlot()[1])
+		return ("{event : %s, area : %s, room : %s, block : %s,  slot : %s-%s} \n" % 
+				(self.getEvent(), self.getArea(), self.getRoom(), self.getBlock(), self.getSlot()[0], self.getSlot()[1]))
 
 	def __str__(self):
 		return "{event : %s, area : %s, block : %s }" % (self.getEvent(), self.getArea(), self.getBlock())
@@ -65,6 +66,7 @@ class LocalSearch(object):
 	def build(self):
 
 		s2 = self.s0
+		self.show(s2)
 		while True:
 			s1 = s2
 			n = self.getNeighbors(s1)
@@ -77,69 +79,68 @@ class LocalSearch(object):
 		
 		n = []
 		for i in range(0, self.nmax):
-			ni = self.insertAndDelete(s)
+			ni = None#self.insertAndDelete(s)
 			if ni == None:
 				ni = self.swap(s)
 			n.append(ni)
-
 		return n
 
 	def insertAndDelete(self, s):
 
 		r1 = random.randint(0, len(s)-1)
-		ts = s[r1].getSlot()[1] - s[r1].getSlot()[0]
-		#TODO validate room is bad and insert is bad 
-
-		for i in range(0, len(s) - 1):
-			tsi = s[i].getSlot()[1] - s[i+1].getSlot()[0]
-			if tsi != 0 and ts <= tsi:
-				#TODO is bad
-				temp = s[ts]
-				s.remove(temp)
-				s.insert(i, temp)
-				print "entro"
-				return s
+		sc = cp.copy(s)
+		sc = [i for i in sc if i.getRoom() != sc[r1].getRoom()]
+		while sc:
+			er = filter(lambda i: i.getRoom() == sc[0].getRoom(), sc)
+			for i in range(0, len(er) - 1):
+				if s[r1].getSlot()[0] >= er[i+1].getSlot()[1] and s[r1].getSlot()[1] <= er[i].getSlot()[0]:
+					s[r1].setBlock(er[i].getBlock())
+					s[r1].setRoom(er[i].getRoom())
+					return s
+			sc = [i for i in sc if i.getRoom() != sc[0].getRoom()]
 
 		return None
 
 	def swap(self, s):
 		
-		r1 = random.randint(0, len(s)-1)
-		r2 = random.randint(0, len(s)-1)
-
 		while True:
-			if s[r1].getSlot()[1] - s[r1].getSlot()[0] == s[r2].getSlot()[1] - s[r2].getSlot()[0]:
+			r1 = random.randint(0, len(s)-1)
+			r2 = random.randint(0, len(s)-1)
+			while r1 == r2:
+				r2 = random.randint(0, len(s)-1)
+
+			if s[r1].getBlock() != s[r2].getBlock() and s[r1].getSlot()[1] - s[r1].getSlot()[0] == s[r2].getSlot()[1] - s[r2].getSlot()[0]:
 				temp = []
-				temp.insert(0, s[r1].getEvent())
-				temp.insert(1, s[r1].getArea())
-				s[r2].setEvent(s[r1].getEvent())
-				s[r2].setArea(s[r1].getArea())
-				s[r1].setEvent(temp[0])
-				s[r1].setEvent(temp[1])
+				temp.insert(0, s[r2].getRoom())
+				temp.insert(1, s[r2].getBlock())
+				temp.insert(2, s[r2].getSlot())
+				s[r2].setRoom(s[r1].getRoom())
+				s[r2].setBlock(s[r1].getBlock())
+				s[r2].setSlot(s[r1].getSlot())
+				s[r1].setRoom(temp[0])
+				s[r1].setBlock(temp[1])
+				s[r1].setSlot(temp[2])
 				break
 
-		return s, True
+		return s
 
 	def improve(self, s, n):
-		eTotal = self.getTotalEventsPerArea(s)
-		eMaxForBlock = self.getMaximunEventsPerAreaEachBlock(s)
-		bTotal = self.getTotalBlocksAssignedToEventSameArea(s)
-
-		sCost = self.getCost(eTotal, eMaxForBlock, bTotal)
+		sCost = self.getCost(s)
 		for nk in n:
-			eTotal = self.getTotalEventsPerArea(nk)
-			eMaxForBlock = self.getMaximunEventsPerAreaEachBlock(nk)
-			bTotal = self.getTotalBlocksAssignedToEventSameArea(nk)
-			nk = self.getCost(eTotal, eMaxForBlock, bTotal)
+			nk = self.getCost(nk)
 			if nk < sCost:
 				return nk
 
 		return None
 
-	def getCost(self, eTotal, eMaxForBlock, bTotal):
+	def getCost(self, s):
 		sCost = 0
+		eTotal = self.getTotalEventsPerArea(s)
+		eMaxForBlock = self.getMaximunEventsPerAreaEachBlock(s)
+		bTotal = self.getTotalBlocksAssignedToEventSameArea(s)
+
 		for i in eTotal:
-			sCost = bTotal[i] * eTotal[i] / eMaxForBlock[i]
+			sCost = (bTotal[i] * eTotal[i] / eMaxForBlock[i] ) + sCost
 		return sCost
 
 	def getTotalEventsPerArea(self, s):
@@ -185,19 +186,20 @@ class LocalSearch(object):
 		return totalE
 
 	def show(self, s):
-		print s
+		print s, self.getCost(s)
 
 
 if __name__ == '__main__':
 	t1 = TimeTabling("mate2", "s1", "b3", "L", "CB", (6, 8))
 
-	t2 = TimeTabling("progr", "s1", "b1", "L", "ING", (8, 10))
-	t3 = TimeTabling("mate1", "s1", "b1", "L", "CB", (10, 12))
+	t7 = TimeTabling("mate5", "s2", "b1", "L", "CB", (6, 8))
+	t3 = TimeTabling("mate6", "s2", "b1", "L", "CB", (10, 12))
+
+	t2 = TimeTabling("progr", "s3", "b2", "L", "ING", (6, 8))
+	t6 = TimeTabling("mate4", "s3", "b2", "L", "CB", (8, 9))
+	t4 = TimeTabling("mate1", "s3", "b2", "L", "CB", (14, 16))
+	t5 = TimeTabling("mate3", "s3", "b2", "L", "CB", (13, 14))
 	
-	t4 = TimeTabling("mate1", "s1", "b2", "L", "CB", (14, 16))
-	t5 = TimeTabling("mate1", "s2", "b2", "L", "CB", (13, 14))
-	t6 = TimeTabling("mate1", "s2", "b2", "L", "CB", (8, 9))
-	t7 = TimeTabling("mate1", "s2", "b2", "L", "CB", (6, 8))
 	swap = [t1, t2, t3, t4, t5, t6, t7]
-	nmax = 1
+	nmax = 100
 	LocalSearch(swap, nmax).build()
